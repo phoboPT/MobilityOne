64dbbff7d16205530c9212cd8740d4a0c6af6e9d
PhoboPT <hugo.branco.c@gmail.com>
2022-06-01 14:38:34 +0100

    update

--- orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -1,62 +1,62 @@
 import mongoose from 'mongoose';
 import { Message } from 'node-nats-streaming';
-import { OrderStatus, ExpirationCompleteEvent } from '@sgtickets/common';
+import { OrderStatus, ExpirationCompleteEvent } from '@mobileorg/common-lib';
 import { ExpirationCompleteListener } from '../expiration-complete-listener';
 import { natsWrapper } from '../../../nats-wrapper';
 import { Order } from '../../../models/order';
 import { Route } from '../../../models/route';
 
 const setup = async () => {
-    const listener = new ExpirationCompleteListener(natsWrapper.client);
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
 
-    const ticket = Route.build({
-        id: mongoose.Types.ObjectId().toHexString(),
-    });
-    await ticket.save();
-    const order = Order.build({
-        status: OrderStatus.Created,
-        userId: 'alskdfj',
-        expiresAt: new Date(),
-        ticket,
-    });
-    await order.save();
+  const ticket = Route.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: 'alskdfj',
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
 
-    const data: ExpirationCompleteEvent['data'] = {
-        orderId: order.id,
-    };
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  };
 
-    // @ts-ignore
-    const msg: Message = {
-        ack: jest.fn(),
-    };
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
 
-    return { listener, order, ticket, data, msg };
+  return { listener, order, ticket, data, msg };
 };
 
 it('updates the order status to cancelled', async () => {
-    const { listener, order, data, msg } = await setup();
+  const { listener, order, data, msg } = await setup();
 
-    await listener.onMessage(data, msg);
+  await listener.onMessage(data, msg);
 
-    const updatedOrder = await Order.findById(order.id);
-    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
 it('emit an OrderCancelled event', async () => {
-    const { listener, order, data, msg } = await setup();
+  const { listener, order, data, msg } = await setup();
 
-    await listener.onMessage(data, msg);
+  await listener.onMessage(data, msg);
 
-    expect(natsWrapper.client.publish).toHaveBeenCalled();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 
-    const eventData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
-    expect(eventData.id).toEqual(order.id);
+  const eventData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
+  expect(eventData.id).toEqual(order.id);
 });
 
 it('ack the message', async () => {
-    const { listener, data, msg } = await setup();
+  const { listener, data, msg } = await setup();
 
-    await listener.onMessage(data, msg);
+  await listener.onMessage(data, msg);
 
-    expect(msg.ack).toHaveBeenCalled();
+  expect(msg.ack).toHaveBeenCalled();
 });
--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,23 +1,23 @@
 import { Message } from 'node-nats-streaming';
-import { Subjects, Listener, TicketUpdatedEvent } from '@sgtickets/common';
+import { Subjects, Listener, TicketUpdatedEvent } from '@mobileorg/common-lib';
 import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
-    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-    queueGroupName = queueGroupName;
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
 
-    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-        const ticket = await Ticket.findByEvent(data);
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findByEvent(data);
 
-        if (!ticket) {
-            throw new Error('Ticket not found');
-        }
-
-        const { title, price } = data;
-        ticket.set({ title, price });
-        await ticket.save();
-
-        msg.ack();
+    if (!ticket) {
+      throw new Error('Ticket not found');
     }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
 }
