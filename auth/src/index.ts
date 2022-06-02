91fe5cb2c65847e88353f14dd4a8be3080a26718
PhoboPT <hugo.branco.c@gmail.com>
2022-06-02 09:41:41 +0100

    update cicd

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import 'dotenv/config';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
