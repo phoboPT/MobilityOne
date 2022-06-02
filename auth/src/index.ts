9d8be2187600c0d6d6208428bb1a02a3f02542cc
PhoboPT <hugo.branco.c@gmail.com>
2022-06-02 09:43:21 +0100

    teste

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -20,6 +20,7 @@ const start = async () => {
   }
   try {
     console.log('orders', process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+
     await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed');
